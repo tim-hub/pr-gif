ec4e9f1c58e6caa5d0765988b802b79500684b80
tim-hub <tim_w@outlook.com>
2021-02-17 16:12:16 +1300

    init build random gif for PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { Carousel } from "@giphy/react-components";
+import { Grid } from "@giphy/react-components";
+import ResizeObserver from "react-resize-observer";
+
+const giphyFetch = new GiphyFetch("sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh");
+
+function randomInteger(min=0, max=1000) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// @ts-ignore
+function GridDemo() {
+  const fetchGifs = (offset: number) =>
+    giphyFetch.search("technology xero github git", { offset: randomInteger(offset), limit: 5 });
+  const [width, setWidth] = useState(window.innerWidth);
+  return (
+    <>
+      <Grid
+        fetchGifs={fetchGifs}
+        width={width}
+        columns={3}
+        gutter={6}
+      />
+      <ResizeObserver
+        onResize={({ width }) => {
+          setWidth(width);
+        }}
+      />
+    </>
+  );
+}
+
+function CarouselDemo() {
+  const fetchGifs = () =>
+    giphyFetch.search("technology xero github git", { offset: randomInteger(), limit: 5 });
+  return <Carousel fetchGifs={fetchGifs} gifHeight={200} gutter={6} />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Random PR Gif</h4>
+      <GridDemo />
     </div>
   );
 }
